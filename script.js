24dc6bfd07aa6509df9ec4999ea50a4eb3ed561c
Omkar141998 <onagpure688@gmail.com>
2023-02-17 21:28:23 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // your code here
+// accesss the element
+var form = document.querySelector("form");
+var h3Element = document.querySelector("h3");
+var nameElement = document.querySelector("#name");
+var yearElement = document.querySelector("#year");
+var btn = document.querySelector("#button");
+
+//Create Function 
+function handleSubmitForm (event) {
+    event.preventDefault() 
+      var h3Value = "https://localhost:8080/";
+      var name = nameElement.value;
+      var year = yearElement.value; 
+
+      if(name && year){
+        h3Value += '?name=' + name + '&year=' + year;
+      }
+      else if (name && !year){
+        h3Value += '?name=' + name;
+      }
+      else if (!name && year){
+        h3Value += '?year=' + year;
+      }
+      h3Element.textContent = h3Value;
+}
+form.addEventListener('submit', handleSubmitForm);
